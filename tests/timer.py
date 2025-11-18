55c676f4ee22eabb49b873b0fd8b13a9dbd3658b
Michal <01187288@pw.edu.pl>
2025-11-18 17:52:24 +0100

    Added timer class

---
+++ tests/timer.py
@@ -0,0 +1,27 @@
+import time
+import gc
+from typing import Callable, Any
+
+class Timer:
+    def __init__(self, func: Callable[[Any], Any], gc_disable: bool = True):
+        self.func = func
+        self.gc_disable = gc_disable
+        self.elapsed = None
+        self._gc_old = None
+
+    def run(self, data: Any) -> Any:
+        self._gc_old = gc.isenabled()
+        if self.gc_disable:
+            gc.disable()
+        start = time.process_time()
+        result = self.func(data)
+        stop = time.process_time()
+        if self.gc_disable and self._gc_old:
+            gc.enable()
+        self.elapsed = stop - start
+        return result
+
+    def get_elapsed(self) -> float:
+        if self.elapsed is None:
+            raise ValueError("Function has not been run yet")
+        return self.elapsed

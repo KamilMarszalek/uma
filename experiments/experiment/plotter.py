dff8568633abc8c7ed2527ba4e3765c9ee7b795f
KamilMarszalek <kamilmar553@gmail.com>
2026-01-23 18:53:26 +0100

    feat: add plotter

---
+++ experiments/experiment/plotter.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+@dataclass(frozen=True, slots=True)
+class PlotterOptions:
+    title: str | None = None
+    x_label: str | None = None
+    y_label: str | None = None
+    output_path: str | None = None
+
+
+def plot_line_chart(
+    data: pd.DataFrame,
+    x_col: str,
+    y_col: str,
+    options: PlotterOptions | None = None,
+) -> None:
+    plt.figure(figsize=(10, 6))
+    plt.plot(data[x_col], data[y_col], marker="o")
+
+    if options is not None:
+        if options.title:
+            plt.title(options.title)
+        if options.x_label:
+            plt.xlabel(options.x_label)
+        if options.y_label:
+            plt.ylabel(options.y_label)
+
+    plt.grid(True)
+
+    if options and options.output_path:
+        plt.savefig(options.output_path)
+    else:
+        plt.show()
+
+    plt.close()
